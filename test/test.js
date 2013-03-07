f2a4c671f43a479e43b1844b6f01f231694570a1
Baptiste Fontaine <batifon@yahoo.fr>
2013-03-07 17:27:42 +0100

    Hex3 & Hex4 tests.

--- test/test.js
+++ test/test.js
@@ -17,7 +17,7 @@ describe( 'Settings', function() {
 
 });
 
-describe( 'Colors parsing', function() {
+describe( 'Parsing', function() {
 
     beforeEach(function() {
 
@@ -25,7 +25,7 @@ describe( 'Colors parsing', function() {
 
     });
 
-    describe( 'CSS/SVG color names', function() {
+    describe( 'of CSS/SVG color names', function() {
 
         it( 'should normalize strings with spaces before', function() {
 
@@ -63,6 +63,84 @@ describe( 'Colors parsing', function() {
             expect( Pheasant.parse( 'blue'    ).toString() ).to.equal( '#00f' );
             expect( Pheasant.parse( 'white'   ).toString() ).to.equal( '#fff' );
             expect( Pheasant.parse( 'fuchsia' ).toString() ).to.equal( '#f0f' );
+
+        });
+
+    });
+
+    describe( 'of hexadecimal strings of length 3', function() {
+
+        it( 'should normalize strings with spaces before', function() {
+
+            expect( Pheasant.parse( '  #abc' ).toString() ).to.equal( '#abc' );
+
+        });
+
+        it( 'should normalize strings with spaces after', function() {
+
+            expect( Pheasant.parse( '#acc   ' ).toString() ).to.equal( '#acc' );
+
+        });
+
+        it( 'should normalize strings with spaces both before and after', function() {
+
+            expect( Pheasant.parse( '  #0ce   ' ).toString() ).to.equal( '#0ce' );
+            expect( Pheasant.parse( ' #110    ' ).toString() ).to.equal( '#110' );
+
+        });
+
+        it( 'should normalize mixed-case strings', function() {
+
+            expect( Pheasant.parse( '#AAA' ).toString() ).to.equal( '#aaa' );
+            expect( Pheasant.parse( '#0Ed' ).toString() ).to.equal( '#0ed' );
+
+        });
+
+
+        it( 'should handle hex3 values', function() {
+
+            expect( Pheasant.parse( '#aaa' ).toString() ).to.equal( '#aaa' );
+            expect( Pheasant.parse( '#fff' ).toString() ).to.equal( '#fff' );
+            expect( Pheasant.parse( '#012' ).toString() ).to.equal( '#012' );
+
+        });
+
+    });
+
+    describe( 'of hexadecimal strings of length 6', function() {
+
+        it( 'should normalize strings with spaces before', function() {
+
+            expect( Pheasant.parse( '  #abcdef' ).toString() ).to.equal( '#ace' );
+
+        });
+
+        it( 'should normalize strings with spaces after', function() {
+
+            expect( Pheasant.parse( '#ab0def   ' ).toString() ).to.equal( '#a0e' );
+
+        });
+
+        it( 'should normalize strings with spaces both before and after', function() {
+
+            expect( Pheasant.parse( '  #a0a0fe   ' ).toString() ).to.equal( '#aaf' );
+            expect( Pheasant.parse( ' #110ccc    ' ).toString() ).to.equal( '#10c' );
+
+        });
+
+        it( 'should normalize mixed-case strings', function() {
+
+            expect( Pheasant.parse( '#BAAb23' ).toString() ).to.equal( '#ba2' );
+            expect( Pheasant.parse( '#0EdCdD' ).toString() ).to.equal( '#0dd' );
+
+        });
+
+
+        it( 'should handle hex6 values', function() {
+
+            expect( Pheasant.parse( '#abcdef' ).toString() ).to.equal( '#ace' );
+            expect( Pheasant.parse( '#123456' ).toString() ).to.equal( '#135' );
+            expect( Pheasant.parse( '#424242' ).toString() ).to.equal( '#444' );
 
         });
 
