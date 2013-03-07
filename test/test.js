8ebe42cf27f6f41dce74b1d909ec4e2e85e8f446
Baptiste Fontaine <batifon@yahoo.fr>
2013-03-07 17:56:14 +0100

    toString hex3 & hex6 tests.

--- test/test.js
+++ test/test.js
@@ -6,12 +6,16 @@ var Pheasant = require( __dirname + '/../src/pheasant' ).Pheasant,
 
 describe( 'Settings', function() {
 
-    it( 'should change the default formatting after a call to .setDefaultStringFormat', function() {
+    describe( 'Pheasant.setDefaultStringFormat', function() {
 
-        expect( Pheasant.parse( '#000' ).toString( 'hex3' ) ).to.equal( '#000' );
-        Pheasant.setDefaultStringFormat( 'hex6' );
-        expect( Pheasant.parse( '#000' ).toString() ).to.equal( '#000000' );
-        expect( Pheasant.parse( '#000' ).toString( 'hex3' ) ).to.equal( '#000' );
+        it( 'should change the default formatting', function() {
+
+            expect( Pheasant.parse( '#000' ).toString( 'hex3' ) ).to.equal( '#000' );
+            Pheasant.setDefaultStringFormat( 'hex6' );
+            expect( Pheasant.parse( '#000' ).toString() ).to.equal( '#000000' );
+            expect( Pheasant.parse( '#000' ).toString( 'hex3' ) ).to.equal( '#000' );
+
+        });
 
     });
 
@@ -141,6 +145,94 @@ describe( 'Parsing', function() {
             expect( Pheasant.parse( '#abcdef' ).toString() ).to.equal( '#ace' );
             expect( Pheasant.parse( '#123456' ).toString() ).to.equal( '#135' );
             expect( Pheasant.parse( '#424242' ).toString() ).to.equal( '#444' );
+
+        });
+
+    });
+
+});
+
+describe( 'Stringifying', function() {
+
+    describe( 'to Hex3 format (#xyz)', function() {
+
+        beforeEach(function() {
+
+            Pheasant.setDefaultStringFormat( 'hex3' );
+
+        });
+
+        it( 'should normalize colors with values higher than 255', function() {
+
+            expect( new Pheasant.Color( 256, 2, 1 ).toString() ).to.equal( '#f00' );
+            expect( new Pheasant.Color( Infinity, 2, 4242 ).toString() ).to.equal( '#f0f' );
+
+        });
+
+        it( 'should normalize colors with negative values', function() {
+
+            expect( new Pheasant.Color( -1, 42, 1 ).toString() ).to.equal( '#020' );
+            expect( new Pheasant.Color( -Infinity, 2, -4242 ).toString() ).to.equal( '#000' );
+
+        });
+
+        it( 'should normalize colors with float values', function() {
+
+            expect( new Pheasant.Color( 0, 15.99, 1.42 ).toString() ).to.equal( '#010' );
+
+        });
+
+        it( 'should normalize colors with negative float values', function() {
+
+            expect( new Pheasant.Color( 0, -15.99, -1.42 ).toString() ).to.equal( '#000' );
+
+        });
+
+        it( 'should not handle colors with NaN values', function() {
+
+            expect( new Pheasant.Color( 0, NaN, 0 ).toString() ).to.be.null;
+
+        });
+
+    });
+
+    describe( 'to Hex6 format (#abcdef)', function() {
+
+        beforeEach(function() {
+
+            Pheasant.setDefaultStringFormat( 'hex6' );
+
+        });
+
+        it( 'should normalize colors with values higher than 255', function() {
+
+            expect( new Pheasant.Color( 256, 2, 1 ).toString() ).to.equal( '#ff0201' );
+            expect( new Pheasant.Color( Infinity, 2, 4242 ).toString() ).to.equal( '#ff02ff' );
+
+        });
+
+        it( 'should normalize colors with negative values', function() {
+
+            expect( new Pheasant.Color( -1, 42, 1 ).toString() ).to.equal( '#002a01' );
+            expect( new Pheasant.Color( -Infinity, 2, -4242 ).toString() ).to.equal( '#000200' );
+
+        });
+
+        it( 'should normalize colors with float values', function() {
+
+            expect( new Pheasant.Color( 0, 15.99, 1.42 ).toString() ).to.equal( '#001001' );
+
+        });
+
+        it( 'should normalize colors with negative float values', function() {
+
+            expect( new Pheasant.Color( 0, -15.99, -1.42 ).toString() ).to.equal( '#000000' );
+
+        });
+
+        it( 'should not handle colors with NaN values', function() {
+
+            expect( new Pheasant.Color( 0, NaN, 0 ).toString() ).to.be.null;
 
         });
 
